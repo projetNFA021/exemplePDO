7b4185f7661cd4fbb9289ab610f7bf6150306e9a
Amine Talemci <talemci.amine@gmail.com>
2018-05-30 19:53:17 +0200

    Revert "add database info"
    This reverts commit 432283f9fa87681aa20fe810af7a0a1431965a06.

--- PDOConnection.php
+++ PDOConnection.php
@@ -20,7 +20,7 @@ class PDOConnection
     
     public static function getInstance() {
         if (!isset(self::$_instance))
-            self::$_instance = new PDO('mysql:host=monservbdd.mysql.database.azure.com;dbname=ateliernfa021', 'talem.bdd@monservbdd', 'Cnam2018@');
+            self::$_instance = new PDO('mysql:host=mysqlserverexemple.mysql.database.azure.com;dbname=ateliernfa021', 'talem.bdd@mysqlserverexemple', 'Myoujd@88');
             
             return self::$_instance;
     }
